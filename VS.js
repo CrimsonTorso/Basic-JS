d2e87fd7fbb35dc2ac8adad05fb6474593640e51
Crimson Torso <40295915+CrimsonTorso@users.noreply.github.com>
2018-10-03 09:56:31 +0100

    Update VS.js

--- VS.js
+++ VS.js
@@ -1,1 +1,2 @@
-
+var vid = document.getElementByID("%"); //Sets a var to the word 'Vid' & selects the ID of the video to alter.
+vid.volume = 0.2; //Uses the previous var to set the volume to a float of 0.2.
